d25568d3272d868ff2fd95a9cd2e5fc699e1fc19
abhinavjha98 <abhinavjha98ald@gmail.com>
2020-08-31 23:23:34 +0530

    Made changes in event link

--- event_fs.py
+++ event_fs.py
@@ -1,12 +1,20 @@
 import pandas as pd
-import numpy as np
 from google.colab import files
 import requests
 from bs4 import BeautifulSoup
-
 URL = 'https://fst.net.au/events/'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
+ab = soup.find_all("div",{"class": "find-out-more"})
+print(ab)
+i=0
+location=[]
+for x in ab:
+    a = x.find('a')
+    if a is not None:
+      data = x.find('a')
+      location.append(data['href'])
+      i =i+1
 
 e_title=[]
 e_event=[]
@@ -62,6 +70,5 @@ data_event = pd.DataFrame(
      'event_venue': e_venue,
      'event_description':e_description
     })
-
 data_event.to_csv('Data_event.csv',index=False)
 files.download("Data_event.csv")
